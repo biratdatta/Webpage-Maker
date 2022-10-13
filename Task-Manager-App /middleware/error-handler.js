a95f6b31d370eb2b3a0ddebe350c9889921c9d24
Louie Rosero <roserolouie@gmail.com>
2022-10-13 09:59:06 +0800

    Adding Mansory Layout #191

--- Task-Manager-App /middleware/error-handler.js
+++
@@ -1,8 +0,0 @@
-const { CustomAPIError } = require('../errors/custom-error');
-const errorHandlerMiddleware = (err, req, res, next) => {
-    if (err instanceof CustomAPIError) {
-        return res.status(err.statusCode).json({msg:err.message})
-    }
-    return res.status(err.status).json({ msg: err }); 
-}
-module.exports = errorHandlerMiddleware;
--- Task-Manager-App /middleware/not-found.js
+++
@@ -1,2 +0,0 @@
-const notFound = (req, res) => res.status(404).send('Route does not exist');
-module.exports = notFound;
