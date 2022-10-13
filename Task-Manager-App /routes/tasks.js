77e5316cd00eab609af22bbd106ebc47c022dc5b
Dinisha Agarwal <63841110+Dinisha18@users.noreply.github.com>
2022-10-13 11:49:05 +0530

    contact us form added component

--- Task-Manager-App /routes/tasks.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const {getAllTasks,createTask,getTask,updateTask,deleteTask}=require('../controllers/tasks')
-router.route('/').get(getAllTasks).post(createTask);
-router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask)
-
-module.exports = router;
