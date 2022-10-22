e0496387dfde577074faae08b94460b97750cde6
fanjasoaAndriann <99532034+fanjasoa18@users.noreply.github.com>
2022-10-22 05:39:33 +0300

    feat: add basic image uploader

---
+++ ImageUploader/script.js
@@ -0,0 +1,8 @@
+const image_input = document.querySelector("#image-input");image_input.addEventListener("change", function() {
+  const reader = new FileReader();
+  reader.addEventListener("load", () => {
+    const uploaded_image = reader.result;
+    document.querySelector("#display-image").style.backgroundImage = `url(${uploaded_image})`;
+  });
+  reader.readAsDataURL(this.files[0]);
+});
